4d8e27c020b4872bb50132db2cbae54960acd40d
ThanhDatNguyen <thanhdat.nguyen2104@gmail.com>
2020-10-22 14:26:19 +0700

    'add-gitignore-enviroment-file'

--- .history/config/db/database_20201022140542.js
+++
@@ -1,9 +0,0 @@
-require('./environment');
-const AWS = require('aws-sdk');
-
-AWS.config.update({
-    region: 'ap-southeast-1',
-    accessKeyId: ACCESS_KEY_ID,
-    secretAccessKey: SECRET_ACCESS_KEY,
-    endpoint: 'http://localhost:8000',
-});
